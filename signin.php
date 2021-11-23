aa5eb0e28c61c3aa4ef0bf120f255bcc0b1fd401
Justin <89216592+JustinK72@users.noreply.github.com>
2021-11-22 21:55:14 -0600

    Update signin.php

--- signin.php
+++ signin.php
@@ -2,8 +2,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3pro.css">
 <style>
 .container {
   position: relative;
